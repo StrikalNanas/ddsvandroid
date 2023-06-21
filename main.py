825ff25959ec61e0e8eb5628d53f1a3d54349b81
StrikaloNanas <zunya2212@gmail>
2023-06-22 03:35:41 +0400

    auto apk

--- main.py
+++ main.py
@@ -1,76 +1,37 @@
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+import kivy
+from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.app import MDApp
-from kivymd.uix.behaviors import DragBehavior
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDFlatButton
-
-kv = '''
-<DraggableButton>:
-    canvas.before:
-        PushMatrix
-        Translate:
-            xy: self.pos
-    canvas.after:
-        Translate:
-            xy: -self.pos
-        PopMatrix
-
-FloatWindow:
-    BoxLayout:
-        orientation: "vertical"
-        size_hint: None, None
-        size: 200, 150
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-
-        MDRaisedButton:
-            text: "Click me!"
-            on_release: app.button_clicked()
-
-'''
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 
 
-class DraggableButton(DragBehavior, MDFlatButton):
-    pass
-
-
-class FloatWindow(FloatLayout):
+class FloatingWindow(BoxLayout):
     def __init__(self, **kwargs):
-        super(FloatWindow, self).__init__(**kwargs)
-        self.add_widget(DraggableButton(text='Click me!', pos_hint={"center_x": 0.5, "center_y": 0.5}))
+        super(FloatingWindow, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.padding = 10
+        self.spacing = 10
+        self.border = (5, 5, 5, 5)  # Добавляем рамку
+        self.button = Button(text='Click me!', size_hint=(0.2, 0.2))
+        self.add_widget(self.button)
 
+    def on_touch_move(self, touch):
+        if self.collide_point(*touch.pos):
+            self.parent.pos_hint = {'x': touch.x / Window.width, 'y': touch.y / Window.height}
 
-class MainScreen(Screen):
-    pass
-
-
-class MyApp(MDApp):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.screen_manager = ScreenManager()
-        self.screen_manager.add_widget(MainScreen(name="main"))
-
-    def build(self):
-        return Builder.load_string(kv)
-
-    def on_start(self):
-        self.open_float_window()
-
-    def open_float_window(self):
-        float_window = FloatWindow()
-        self.float_window_screen = MDScreen()
-        self.float_window_screen.add_widget(float_window)
-        self.screen_manager.add_widget(self.float_window_screen)
-        self.screen_manager.current = "main"
-
-    def button_clicked(self):
+    def button_clicked(self, instance):
         print("Button clicked!")
+
+
+class MyApp(App):
+    def build(self):
+        Window.size = (300, 200)
+        Window.top = 100
+        Window.left = 100
+        Window.borderless = False  # Отключаем полноэкранный режим
+
+        floating_window = FloatingWindow()
+        return floating_window
 
 
 if __name__ == '__main__':
